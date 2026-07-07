0877530e20e38dd8af32b34040173f26c130ee63
Harshith-yogesh <hharshithy@gmail.com>
2026-07-07 12:10:06 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+          int max=0;
+           int sum=0;
+          for(int i=0;i<prices.length;i++){
+              for(int j=i+1;j<prices.length;j++){
+                  if(prices[j]>prices[i]){
+                      sum=prices[j]-prices[i];
+                      max=Math.max(max,sum);
+                  }
+              }
+          }
+          return  max;
+
+    }
+}
